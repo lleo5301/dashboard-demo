b681c75633bb2f2fd6eec969060f9e5562770416
leonardo Quintero <lleo5301@gmail.com>
2015-11-18 18:38:04 -0500

    aggregate comments and user routes

--- config/routes.js
+++ config/routes.js
@@ -12,10 +12,48 @@ router.use(function timeLog(req, res, next) {
 router.get('/', function(req, res) {
   res.send('nothing here');
 });
-// define comments route
-router.get('/comments', function(req, res) {
-  
+
+
+// basic crud
+router.get('/comments', function(req,res){
+	comments.get(req.query, function(err, data){
+		res.json(data);
+	})
+})
+
+router.get('/users', function(req,res){
+	comments.redditUsers(function(err, data){
+		res.json(data);
+	})
+})
+
+
+
+
+
+
+
+
+//aggregates
+router.get('/controversial', function(req, res){
+	comments.controversial(function(err, data){
+		res.json(data);
+	})
 });
+
+router.get('/subredditaggregate', function(req,res){
+	console.log('subredditaggregate');
+	comments.subredditAggregate(function(err, data){
+		res.json(data);
+	})
+})
+
+router.get('/userAggregate', function(req, res){
+	console.log('userAggregate');
+	comments.userAggregate(function(err, data){
+		res.json(data);
+	})
+})
 
 
 
