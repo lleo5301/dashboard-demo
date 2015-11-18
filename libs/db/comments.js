9fd44251ae7440fc3c070ea6309d65fbcd697f93
leonardo Quintero <lleo5301@gmail.com>
2015-11-18 18:39:21 -0500

    aggregate user and comment functions including pagination

--- libs/db/comments.js
+++ libs/db/comments.js
@@ -1,24 +1,105 @@
 var Comments = require('../../models/comments.js');
 
 module.exports = {
-	get:function(start, end, callback){
-		var start = start.toString();
-		var end = end.toString();
-
-		callback(null);
+	get:function(rquery,callback){
+		// console.log(page, limit);
+		console.log(rquery);
+		var query = {};
+		var page = 1;
+		var limit = 100;
+		var skip = 0;
+		//parse query
+		if(rquery.user){
+			query.author = rquery.user;
+		}
+		if(rquery.page > 1){
+			page = 2;
+			skip = (page-1) * limit;
+		}
+		Comments.find(query).count(function(err, count){
+			Comments.find(query).sort({_id:-1}).skip(skip).limit(limit).exec(function(err, data){
+				// console.log(err,data);
+				data=data.map(function(comment){
+					// console.log(comment.created_utc);
+					date = new Date(comment.created_utc * 1000);
+					console.log(date);
+					return {comment:comment.body, 
+							date:date, 
+							subreddit:comment.subreddit,
+							distinguished:comment.distinguished,
+							ups: comment.ups,
+							controversiality: comment.controversiality};
+				});
+				data = {meta:{total_records:count, page:page, total_pages:Math.floor(count/limit)}, results:data}
+				returnCallback(err, data, callback);
+			})
+		})
+		
 	},
 	controversial:function(callback){
 		//build aggregate
 		var aggregate =  [
-		{$group:{_id:'$controversiality'}, total:{$sum:1}}
+		{$match:{controversiality:{$gt:100}}},
+		{$group:{_id:'$controversiality',total:{$sum:1}}},
+		{$limit:100}
 		]
 
-		Comments.aggregate(aggregate).exec(function(err, data){
-			if(err){
-				callback(err, null);
-			}else{
-				callback(null, data);
-			}
+		Comments.aggregate(aggregate).exec(function(err,data){
+			returnCallback(err,data,callback);
+		});
+	},
+	subredditAggregate:function(callback){
+		var aggregate = [
+			{$limit:1000000},
+			{$match:{subreddit:{$ne:null}}},
+			{$group:{_id:'$subreddit', total:{$sum:1}}},
+			{$sort:{total:-1}},
+			{$match:{total:{$gte:10000}}},
+			{$limit:5}
+		]
+		
+		Comments.aggregate(aggregate).exec(function(err,data){
+			returnCallback(err,data,callback);
+		});
+	},
+	redditUsers:function(callback){
+		var aggregate = [
+			{$limit:100000},
+			{$group:{_id:'$author'}},
+			{$project:{user:'$_id', _id:0}}
+		];
+
+		Comments.aggregate(aggregate).exec(function(err,data){
+			returnCallback(err,data,callback);
+		});
+	},
+	userAggregate:function(callback){
+		var aggregate = [
+		{$match:{author:{$ne:"[deleted]"}}},
+		{$limit:5000000},
+		{$group:{_id:'$author', total:{$sum:1}}},
+		{$project:{user:'$_id', total:1, _id:0}},
+		{$sort:{total:-1}},
+		{$limit:100}
+		]
+		Comments.aggregate(aggregate).allowDiskUse(true).exec(function(err, data){
+			returnCallback(err, data, callback);
 		})
 	}
+
 }
+
+
+
+
+//callback function default
+var returnCallback = function(err, data, callback){
+	// console.log(err, data);
+			if(err){
+				console.log(err);
+				callback(err, null);
+			}else{
+				// console.log(data);
+				callback(null, data);
+			}
+};
